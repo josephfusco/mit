0a46a3d0a79d807004d8d3e0fc6719bc0fb3e046
Joseph Fusco <hello@josephfus.co>
2016-01-11 14:45:42 -0500

    Add query for year.

---
+++ main.js
@@ -0,0 +1,12 @@
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+      results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+// get year from query string
+var year = getParameterByName('year');
+
+// print year to license
+document.getElementById("year").innerHTML = year;
